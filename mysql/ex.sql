f4734839b1524c990272a06e93342a5fe73e4cf0
123123 <15651875728@163.com>
2019-05-16 23:51:32 +0800

    complete dbcourse ex

--- mysql/ex.sql
+++ mysql/ex.sql
@@ -1,3 +1,4 @@
+-- 实验一：SQL 定义功能、数据插入 1 学时
 -- 创建学生数据库
 create database dbcourse;
 
@@ -49,8 +50,13 @@ insert into SC values ('201215121', '1', 92);
 insert into SC values ('201215121', '2', 85);
 insert into SC values ('201215121', '3', 88);
 insert into SC values ('201215122', '2', 90);
-insert into SC values ('201215122', '3', 80);
+insert into SC values ('201215122', '6', 80);
+insert into SC values ('201215122', '7', 90);
+insert into SC values ('201215122', '5', 90);
+insert into SC values ('201215122', '1', 90);
 
+
+-- 实验二：数据查询 2 学时
 -- 1．查询选修 1 号课程的学生学号与姓名。
 select s.sname, s.sno 
 from s, sc
@@ -74,20 +80,127 @@ where s.sno not in (
     where sc.cno = '1'
 );
 
--- 4．查询学习全部课程学生姓名。
-
-
+-- 4．查询学习全部课程学生姓名。(1 2 3课程)
+select s.sname
+from s
+where not exists (
+    select *
+    from c
+    where c.cno in ('1', '2', '3') and not exists (
+        select *
+        from sc
+        where sc.sno = s.sno and sc.cno = c.cno
+    )
+);
 
 -- 5．查询所有学生除了选修 1 号课程外所有成绩均及格的学生的学号和平均成绩，其结果按平均成绩的降序排列。
+select x.sno, avg(x.Grade)
+from sc x
+where x.sno in (
+    select y.sno
+    from sc y
+    where not exists (
+        select *
+        from sc z
+        where z.sno = y.sno and z.cno != 1 and z.Grade < 60 
+    )
+)
+group by x.sno
+order by avg(x.Grade) desc;
 
+-- 6．查询选修数据库原理成绩第 2 名的学生姓名。 （换成数学课）
+select s.sname
+from s, sc, c
+where s.sno = sc.sno and sc.cno = c.cno and c.cname = '数学'
+order by sc.Grade desc
+limit 1, 1;
 
--- 6．查询选修数据库原理成绩第 2 名的学生姓名。
-
-
--- 7. 查询所有 3 个学分课程中有 3 门以上（含 3 门）课程获 80 分以上（含 80 分）的学生的姓名。
-
+-- 7. 查询所有 3(改成4) 个学分课程中有 3 门以上（含 3 门）课程获 80 分以上（含 80 分）的学生的姓名。
+select s.sname
+from s
+where s.sno in (
+    select sc.sno
+    from c, sc
+    where sc.cno = c.cno and c.Ccredit = 4 and sc.Grade >= 80
+    group by sc.sno
+    having count(sc.sno) >= 3
+);
 
 -- 8. 查询选课门数唯一的学生的学号。
+create view sc_count(sno, c_count)
+as
+select sc.sno, count(sc.cno)
+from sc
+group by sc.sno;
 
+select x.sno
+from sc_count x
+where x.sno not in (
+    select y.sno
+    from sc_count y
+    where y.sno != x.sno and y.c_count = x.c_count
+);
 
 -- 9．SELECT 语句中各种查询条件的实验。
+
+
+-- 实验三：数据修改、删除 1 学时
+-- 1．把 1 号课程的非空成绩提高 10％。
+update sc
+set sc.Grade = sc.Grade * 1.1
+where sc.cno = '1' and sc.Grade is not null;
+
+-- 2．在 SC 表中删除课程名为数据结构的成绩的元组。
+delete from sc
+where sc.cno in (
+    select c.cno
+    from c
+    where c.cname = '数据结构'
+);
+
+-- 3．在 S 和 SC 表中删除学号为 201215122 的所有数据。
+delete from sc
+where sc.sno = '201215122';
+delete from s
+where s.sno = '201215122';
+
+
+-- 实验四：视图的操作 1 学时
+-- 1．建立男学生的视图，属性包括学号、姓名、选修课程名和成绩。
+create view student_male
+as
+select s.sno, s.sname, c.cname, sc.Grade
+from s, sc, c
+where s.sno = sc.sno and sc.cno = c.cno and s.Ssex = '男';
+
+-- 2．在男学生视图中查询平均成绩大于 80 分的学生学号与姓名。
+select m.sno, m.sname
+from student_male m
+group by m.sno
+having avg(m.Grade) > 80;
+
+
+-- 实验五：库函数，授权控制 1 学时
+-- 1．计算每个学生有成绩的课程门数、平均成绩。
+select sc.sno, count(sc.cno), avg(sc.Grade)
+from sc
+group by sc.sno;
+
+-- 2．使用 GRANT 语句，把对基本表 S、SC、C 的使用权限授给其它用户。
+/* 查看用户 */
+select host,user, plugin from mysql.user;
+/* 创建用户 - 只需要在 mysql 数据库中的 user 表添加新用户即可。 */
+grant usage 
+on *.* to 'guest'@'localhost' identified by '123456' 
+with grant option;
+/* 授权 */
+grant select 
+on *.* 
+to guest@"localhost";
+
+-- 3．实验完成后，撤消建立的基本表和视图。
+drop view if exists student_male;
+drop view if exists sc_count;
+drop table if exists S;
+drop table if exists C;
+drop table if exists SC;
